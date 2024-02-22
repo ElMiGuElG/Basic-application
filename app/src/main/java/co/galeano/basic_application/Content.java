5283600790580c1aa8fcd01b0e0395f33aefd116
Miguel Ángel Galeano <113398998+ElMiGuElG@users.noreply.github.com>
2024-02-22 00:11:25 -0500

    the finish has been added

--- app/src/main/java/co/galeano/basic_application/Content.java
+++ app/src/main/java/co/galeano/basic_application/Content.java
@@ -92,6 +92,7 @@ public class Content extends AppCompatActivity {
             public void onClick(View view) {;
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
